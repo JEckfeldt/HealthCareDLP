d30b9f51a6fe44545d78c56a2de3af3d04290b3e
jeckfeld <jacob.eckfeldt@wsu.edu>
2023-10-06 12:19:37 -0700

    patient and doctor model outline

--- HealthCareForm/DoctorPortal/models.py
+++ HealthCareForm/DoctorPortal/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 # Patient model
+
+class Doctor(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+
+class Patient(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
